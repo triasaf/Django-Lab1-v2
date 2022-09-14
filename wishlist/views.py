b5e5a7e3994315eb3fff5f5fded5418cb7f13eb5
Trias Ahmad Fairuz <trias3011@gmail.com>
2022-09-14 09:48:55 +0700

    corrected views.py for wishlist app

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
+data_barang_wishlist = BarangWishlist.objects.all()
+context = {
+'list_barang': data_barang_wishlist,
+'nama': 'Triazz'
+}
+
 # Create your views here.
 def show_wishlist(request):
-    data_barang_wishlist = BarangWishlist.objects.all()
-    context = {
-    'list_barang': data_barang_wishlist,
-    'nama': 'Triazz'
-    }
+
     return render(request, "wishlist.html", context)
 
