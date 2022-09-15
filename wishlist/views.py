cce1ae37b63ad3c8bd45e8711a3e6cbb83d62d66
Trias Ahmad Fairuz <trias3011@gmail.com>
2022-09-15 13:16:02 +0700

    added show xml and json for wishlist app

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
+
 
 data_barang_wishlist = BarangWishlist.objects.all()
 context = {
@@ -11,4 +14,19 @@ context = {
 def show_wishlist(request):
 
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    return HttpResponse(serializers.serialize("xml", data_barang_wishlist), content_type="application/xml")
+
+def show_json(request):
+    return HttpResponse(serializers.serialize("json", data_barang_wishlist), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    
 
