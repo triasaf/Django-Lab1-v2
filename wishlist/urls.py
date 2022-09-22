f247efc09ee2bbaa1384f72844c57139d80eb76a
Trias Ahmad Fairuz <trias3011@gmail.com>
2022-09-22 14:04:37 +0700

    login logout and cookies

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,6 +4,9 @@ from wishlist.views import show_xml
 from wishlist.views import show_json 
 from wishlist.views import show_json_by_id
 from wishlist.views import show_xml_by_id
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
@@ -11,6 +14,10 @@ urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
+
 ]
